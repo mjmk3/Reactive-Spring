cd6873b0d65f74414e11803fd2bc76deb0ff35c5
Mohamad J. Makki <malanbaki@transforat.com>
2022-12-25 10:37:54 +0300

    finishing up the reactive spring project for testing

--- src/main/java/app/reactive/reactivesp/ReactiveSpApplication.java
+++ src/main/java/app/reactive/reactivesp/ReactiveSpApplication.java
@@ -2,8 +2,10 @@ package app.reactive.reactivesp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication
+@EnableReactiveMongoRepositories
 public class ReactiveSpApplication {
 
     public static void main(String[] args) {
